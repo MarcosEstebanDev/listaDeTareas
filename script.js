7dab92f25c5f60ddf5847a4161902328ee35a876
MarcosGodoydevelopped <83374363+MarcosGodoydevelopped@users.noreply.github.com>
2021-11-23 01:53:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const dateText = document.getElementById('dateText');
 const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
 
-//taks container
+//taks container, donde alojamos las tareas
 const tasksContainer = document.getElementById('tasksContainer');
 
-//Setear la fecha actual
+//Setear la fecha actual, modificamos los datos que nos devuelve Date.
 const setDate = () => {
     const date = new Date();
     dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
@@ -17,30 +17,46 @@ const setDate = () => {
 
 };
 //evento para agregar nueva tarea
+
 const addNewTask = event => {
+
 //evitar el submit
     event.preventDefault();
+    
 //accedemos con el target del evento
+
     const { value } = event.target.taskText;
     if (!value) return;
+    
+    
     const task = document.createElement('div');
     task.classList.add('task', 'roundBorder');
+    
     task.addEventListener('click', changeTaskState)
     task.textContent = value;
+    //agregamos al principio de la lista
     tasksContainer.prepend(task);
     event.target.reset();
 };
+
+
+//Cambiamos el estado de hecha por hacer
 const changeTaskState = event => {
     event.target.classList.toggle('done');
 };
+
+//Ordenamos las tareas 
 const order = () => {
     const done = [];
     const toDo = [];
+    
+    //aceedemos a las tareas y iteramos
     tasksContainer.childNodes.forEach(el => {
         el.classList.contains('done') ? done.push(el) : toDo.push(el)
     })
     return [...toDo, ...done];
 }
+//Iteramos cada elemento del array y lo ubicamos en el container
 const renderOrderedTasks = () => {
     order().forEach(el => tasksContainer.appendChild(el))
 }
